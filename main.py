fe829bf8c9021055f7f98a8adb31b4cb14ccba65
lricciar <64287801+lricciar@users.noreply.github.com>
2021-09-25 14:17:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 from matplotlib import pyplot as mp
 import numpy as np
 
-x = np.arange(0,2*np.pi,.1)
-y = np.sin(x)
-z = np.cos(x)
-
-mp.plot(x,y,x,z)
-mp.show()
+def sincos(x,y,z):
+  if __name__ == '__main__':
+    x = np.arange(0,2*np.pi,.1)
+    y = np.sin(x)
+    z = np.cos(x)
+    mp.plot(x,y,x,z)
+    mp.show()
+  
+  
